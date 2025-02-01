5a2350a316b19dbae3c0af69db1ed58c4639dd5c
Henrique Kioshi Yamauchi <2hkioshi2003@gmail.com>
2025-01-31 22:22:35 -0300

    ES - Let e Const

---
+++ EcmaScript/aulas/letEconst.js
@@ -0,0 +1,13 @@
+let variavel = 10;
+//o let é uma variável que pode ser alterada
+
+const constante = 10;
+//o const é uma variável que não pode ser alterada
+
+//a diferença entre o let e o var é que o let é uma variável de 
+//escopo de bloco, enquanto o var é uma variável de escopo de função
+
+//os escopos sao globais, de bloco e de função
+//as globais são acessíveis em qualquer lugar do código
+//as de bloco são acessíveis apenas dentro do bloco
+//as de função são acessíveis apenas dentro da função
